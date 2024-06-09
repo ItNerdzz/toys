8cf3690d5d031d428a4153edb97b535a143cb6ba
VasiliCheb <skimanvasia@gmail.com>
2024-06-09 21:18:46 +0300

    fix(slider-image): правит js для работы нескольких слайдеров

--- src/js/slider-image-swiper.js
+++ src/js/slider-image-swiper.js
@@ -2,41 +2,46 @@ import Swiper from 'swiper';
 import {Navigation} from 'swiper/modules';
 
 
-const sliderImageSwiper = document.querySelector('.slider-image__swiper');
+const slidersImageSwiper = document.querySelectorAll('.slider-image__swiper');
 
 const initSliderImageSwiper = () => {
-  if (sliderImageSwiper) {
-    new Swiper(sliderImageSwiper, {
-      direction: 'horizontal',
-      slidesPerView: 1.698, // демонстрация количества слайдов
-      spaceBetween: 16, // расстояние между слайдами
-      initialSlide: 0, // с какого слайда начать показ
-      autoHeight: true, // автовысота
-      speed: 500, // скорость перелистывания
-      watchOverflow: true, // отключает если недостаок слайдов
 
-      //Адаптив Ширина экрана
-      breakpoints: {
-        720: {
-          slidesPerView: 2.914,
+  slidersImageSwiper.forEach((sliderImageSwiper) => {
+
+    if (sliderImageSwiper) {
+
+      new Swiper(sliderImageSwiper, {
+        direction: 'horizontal',
+        slidesPerView: 1.698, // демонстрация количества слайдов
+        spaceBetween: 16, // расстояние между слайдами
+        initialSlide: 0, // с какого слайда начать показ
+        autoHeight: true, // автовысота
+        speed: 500, // скорость перелистывания
+        watchOverflow: true, // отключает если недостаок слайдов
+
+        //Адаптив Ширина экрана
+        breakpoints: {
+          720: {
+            slidesPerView: 2.914,
+          },
+          1024: {
+            slidesPerView: 4.308,
+          },
+          1440: {
+            slidesPerView: 5.931,
+          }
         },
-        1024: {
-          slidesPerView: 4.308,
+
+
+        modules: [Navigation],
+
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
-        1440: {
-          slidesPerView: 5.931,
-        }
-      },
-
-
-      modules: [Navigation],
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  }
+      });
+    }
+  });
 };
 
 export {initSliderImageSwiper};
