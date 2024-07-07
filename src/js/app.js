20909070fa53b9abd142d71567c055ac27a05588
VasiliCheb <skimanvasia@gmail.com>
2024-07-07 21:16:35 +0300

    feat(sale): создает модалки и добавляет стили

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+import {mobileVhFix} from './utils/mobile-vh-fix.js';
+import {initModals} from './init-modals';
 import {initIntroSwiper} from './intro-swiper';
 import {initAdvantagesSwiper} from './advantages-swiper';
 import {initSliderImageSwiper} from './slider-image-swiper';
@@ -14,13 +16,33 @@ import {initSliderCardSwiper} from './slider-card-swiper';
 import {initGratitudeSwiper} from './gratitude-swiper';
 import {initTabs} from './tabs';
 
-initIntroSwiper();
-initAdvantagesSwiper();
-initSliderImageSwiper();
-initСertificatesSwiper();
-initCooperationSwiper();
-initPopularSwiperMobile();
-initCardProductSwiper();
-initSliderCardSwiper();
-initGratitudeSwiper();
-initTabs();
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  mobileVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  initIntroSwiper();
+  initAdvantagesSwiper();
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+
+    initModals();
+
+    initAdvantagesSwiper();
+    initSliderImageSwiper();
+    initСertificatesSwiper();
+    initCooperationSwiper();
+    initPopularSwiperMobile();
+    initCardProductSwiper();
+    initSliderCardSwiper();
+    initGratitudeSwiper();
+    initTabs();
+  });
+});
