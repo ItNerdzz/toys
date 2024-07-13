d540d774fd96d58089aaecb267e02b0f891c6b5f
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-07-13 13:42:40 +0400

    Fixes

--- src/js/callback.js
+++ src/js/callback.js
@@ -3,10 +3,14 @@ import {handleOpenPopup} from "./popup.js";
 const callbackPopup = document.querySelector('.callback-popup');
 const callbackButtons = document.querySelectorAll('.callback-button');
 
+const handleCallbackButton = () => {
+  handleOpenPopup(callbackPopup);
+};
+
+window.handleCallbackButton = handleCallbackButton;
+
 if (callbackPopup && callbackButtons) {
   callbackButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      handleOpenPopup(callbackPopup);
-    });
+    button.addEventListener('click', handleCallbackButton);
   });
 }
